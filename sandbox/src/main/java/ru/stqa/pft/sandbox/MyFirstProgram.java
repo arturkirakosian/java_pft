bfa3107197434a72c915b2abac852b1f57c27a78
arturkirakosian <artur.kirakosyan@icloud.com>
2019-10-24 18:46:21 +0300

    Создан пакет для моей первой программы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
+package ru.stqa.pft.sandbox;
+
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello, yogin!");
 	}
 }
